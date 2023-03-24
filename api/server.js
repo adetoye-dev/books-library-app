06036120e20ce7b9d25d919a642eb59db1a5c889
Remi-dee <oluwaremidaniel36@gmail.com>
2023-03-24 16:43:28 -0700

    Connecting Database

--- api/server.js
+++ api/server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-const {errorHandler} = require("./middleware/middleware")
+const { errorHandler } = require("./middleware/middleware");
+
+const connectDB = require("./config/db");
+
+connectDB();
+
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/books", require("./routes/booksRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
