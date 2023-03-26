05aa9af7be01ebe0d91fd2e550c8b712a4e67e6b
Remi-dee <oluwaremidaniel36@gmail.com>
2023-03-26 16:36:03 -0700

    Connecting Database

---
+++ api/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  //there is a bug to fix here
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://Book-library:PhaBfqeAMijRnp18@remicluster.kyndevf.mongodb.net/?retryWrites=true&w=majority"
+    )
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
