c202b9ce9b51b979e2820dfa4e6235400c81b3be
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-27 01:36:48 +0100

    Add mogodb uri to .env

--- api/config/db.js
+++ api/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const connectDB = async () => {
   //there is a bug to fix here
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://Book-library:e6yI4h87bD5jotbK@remicluster.kyndevf.mongodb.net/?retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
