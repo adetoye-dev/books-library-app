253ef46c3d3ae1c9ab1294adce8518f98f777fae
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-04 20:14:43 +0100

    Allow cors access for client url

--- api/server.js
+++ api/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/middleware");
 const app = express();
 const colors = require("colors");
 const connectDB = require("./config/db");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
+dotenv.config();
 connectDB();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.urlencoded({ extended: false }));
