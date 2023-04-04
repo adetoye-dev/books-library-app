2cc4c780fa321d21192d932f73db24c10ddb0c2d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-04 20:03:27 +0100

    Fix className error

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,13 +3,13 @@ import heroImg from "../assets/StackOfBooks.svg";
 function Hero() {
   return (
     <div className="w-full min-h-screen text-white bg-gradient-to-r from-teal-500 to-blue-500">
-      <div class="container mx-auto flex px-5 py-20 items-center justify-center flex-col">
+      <div className="container mx-auto flex px-5 py-20 items-center justify-center flex-col">
         <img
-          class="lg:w-1/6 md:w-3/6 w-5/6 mb-10 object-cover object-center"
+          className="lg:w-1/6 md:w-3/6 w-5/6 mb-10 object-cover object-center"
           alt="hero"
           src={heroImg}
         />
-        <div class="text-center lg:w-6/12 w-full">
+        <div className="text-center lg:w-6/12 w-full">
           <h1 className="my-4 text-5xl font-bold leading-tight">
             Looking for a tech book?
           </h1>
