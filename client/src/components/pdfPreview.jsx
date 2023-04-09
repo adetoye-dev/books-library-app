50dad1544781cde5ae5b96b03cde01479be1f275
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-09 16:50:21 +0100

    Update preview Pdf function

--- client/src/components/pdfPreview.jsx
+++ client/src/components/pdfPreview.jsx
@@ -2,8 +2,39 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
+import { extractPreview } from "../util/extractPreview";
+import { useState, useEffect, useCallback } from "react";
+import { Worker } from "@react-pdf-viewer/core";
+// Import the main component
+import { Viewer } from "@react-pdf-viewer/core";
+// Import the styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 export default function PdfPreview({ isOpen, handleClose, pdfUrl }) {
+  const [data, setData] = useState(null);
+
+  const fetchPreview = useCallback(async () => {
+    try {
+      const data = await extractPreview(pdfUrl);
+
+      const reader = new FileReader();
+
+      reader.onloadend = function (e) {
+        setData(e.target.result);
+      };
+
+      reader.readAsDataURL(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [pdfUrl]);
+
+  useEffect(() => {
+    if (isOpen) {
+      fetchPreview();
+    }
+  }, [isOpen, fetchPreview]);
+
   return (
     <Dialog
       fullScreen
@@ -18,12 +49,14 @@ export default function PdfPreview({ isOpen, handleClose, pdfUrl }) {
         </Button>
       </DialogTitle>
       <DialogContent>
-        <iframe
-          src={`${pdfUrl}#view=fitH`}
-          title="Book Title"
-          height="100%"
-          width="100%"
-        />
+        {data ? (
+          // <iframe src={data} width="100%" height="100%" />
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Viewer fileUrl={data} />;
+          </Worker>
+        ) : (
+          <p>Loading Book Preview...</p>
+        )}
       </DialogContent>
     </Dialog>
   );
